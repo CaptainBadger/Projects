0c6e733b56ece4dd029ba9bdd19ef68cb9b19104
CaptainBadger <captainbadger2006@googlemail.com>
2013-07-24 11:06:44 +0100

    Update prime_factors.py
    Added in conditions for n=1 and n=2. Needs testing.

--- Numbers/prime_factors.py
+++ Numbers/prime_factors.py
@@ -3,13 +3,19 @@
 def prime_fac(n):
 	primes = []
 	prime_factors = []
-	for x in xrange[1,n]:
-		for y in xrange[1,x]:
-			if y != 1 and y != x and x % y == 0:
-				break
-			if y=x:
-				primes.append[x]
-	for p in primes:
-		if n % p == 0:
-			prime_factors.append[p]
-	print prime_factors
+	if n == 1:
+		print prime_factors
+	if n == 2:
+		prime_factors.append[2]
+		print prime_factors
+	else:
+		for x in xrange[1,n]:
+			for y in xrange[1,x]:
+				if y != 1 and y != x and x % y == 0:
+					break
+				if y=x:
+					primes.append[x]
+		for p in primes:
+			if n % p == 0:
+				prime_factors.append[p]
+		print prime_factors
