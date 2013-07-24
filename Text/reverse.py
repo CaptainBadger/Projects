379a78425cb0c6fef5e49efc81268fcee9c5a683
CaptainBadger <captainbadger2006@googlemail.com>
2013-07-24 15:50:14 +0100

    Update reverse.py

--- Text/reverse.py
+++ Text/reverse.py
@@ -6,6 +6,6 @@ def reverse(string):
 	while len(new_string) != 0:
 		letter = new_string[:1]
 		new_string = new_string[1:]
-		reverse_string = reverse_string + letter
+		reverse_string = letter + reverse_string
 	print reverse_string
 		
